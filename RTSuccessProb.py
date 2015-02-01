cfd900ba98e5ab135cac4b4eb50e5dc3ef695737
swheaton <stuart@Stuart-Wheatons-Computer-3.local>
2015-02-01 14:23:54 -0500

    RTSuccessProb python script finished
    Now correctly prints out success probability for given RT dimensions.
    Problem before was with integer division instead of decimal division (I
    thought Python converted automatically, guess not)

--- RTSuccessProb.py
+++ RTSuccessProb.py
@@ -1,43 +1,40 @@
 #!/usr/bin/python
 from sys import argv
 from math import exp
+from decimal import Decimal
 
 def findMTerms(m,t,N):
 	mTermList = [m]
 	for i in xrange(1,t):
-		#print N*(1-exp(-1 * mTermList[i-1] / N))
 		mTermList.append(N * (1-exp(-1*mTermList[i-1]/N)))
 	return mTermList
 def productSumTerm(m,t,N):
 	out = 1.0
 	mTerms = findMTerms(m,t,N)
-	#print mTerms
 	for i in xrange(0,t):
-		#print 1-mTerms[i]/N
 		out *= (1 - mTerms[i]/N)
 	return out
 	
-def findKeySpaceSize(charSetSize, maxPasswordLength):
-	size = 0;
+def findKeySpaceSize():
+	charSetSize = int(argv[3])
+	maxPasswordLength = int(argv[4])
+	size = 0.0;
 	for i in xrange(1,maxPasswordLength+1):
 		size += charSetSize**i
 	return size
 	
-def findSuccessProb():
+def findSuccessProb(keySpaceSize):
 	numChains = int(argv[1])
 	chainLength = int(argv[2])
-	charSetSize = int(argv[3])
-	maxPasswordLength = int(argv[4])
-	
-	keySpaceSize = findKeySpaceSize(charSetSize, maxPasswordLength)
 	
 	return 1-productSumTerm(numChains, chainLength, keySpaceSize)
-	
-#print findKeySpaceSize(26,8)
-#exit()
 
 if(len(argv) != 5):
 	print "Usage: python RTSuccessProb.py <numChains> <chainLength> <charSetSize> <maxPasswordLength"
 	exit()
+print "Keyspace size is: "
+keySpace=findKeySpaceSize()
+print int(keySpace)
+print ""
 print "Probability of success for a rainbow table of these dimensions is:"
-print findSuccessProb()
+print findSuccessProb(keySpace)
