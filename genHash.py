edf7b9f9a95d6aecc97f207429e3cc0871774d0f
swheaton <stuart@Stuart-Wheatons-Computer-3.local>
2015-02-15 23:04:46 -0500

    Bug in Python script

--- genHash.py
+++ genHash.py
@@ -3,6 +3,6 @@ from sys import argv
 
 with open(argv[1],'r') as file:
 	for line in file:
-		hash_object = hashlib.sha1(line[0:len(line)])
+		hash_object = hashlib.sha1(line[0:len(line)-1])
 		hex_dig = hash_object.hexdigest()
 		print(hex_dig)
